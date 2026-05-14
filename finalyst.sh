9e4ceed7aff31c7ad01fd7a1369d00a7d77285bf
Mikail Plotzky <70287588+DraftedDev@users.noreply.github.com>
2026-05-14 12:38:32 +0200

    Add helper scripts

--- finalyst.sh
+++ finalyst.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/sh
+
+set BINARY "./target/release/finalyst"
+
+if not test -f $BINARY
+    echo "Release binary not found. Please run 'build-release.sh' first."
+    exit 1
+end
+
 ./target/release/finalyst "$@"
